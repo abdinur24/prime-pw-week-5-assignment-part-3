bd72d7e8fd6caefab64815f48ad33f19820959a7
Abdishakur Nur <abdinur20000@gmail.com>
2022-04-24 17:14:44 -0500

    Finished findByArtist

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -4,25 +4,37 @@ let collection = [];
 
 function addTooCollection(title, artist, yearPublished){
     console.log('In addTooCollection')
-    let music = [
+    let album = [
         {title:'To Pimp A Butterfly', artist: 'Kendrick Lamar', yearPublished: 2015}, 
-        {title: 'Astroworld', artist: 'Travis Scott', yearPublished: 2018}
+        {title: 'Astroworld', artist: 'Travis Scott', yearPublished: 2018},
+        {title: 'Die Lit', artist: 'Playboi Carti', yearPublished: 2018},
+        {title: 'good kid m.A.A.d city', artist: 'Kendrick Lamar', yearPublished: 2012},
+        {title: 'My Turn', artist: 'Lil Baby', yearPublished: 2020},
+        {title: 'My Beautiful Dark Twisted Fantasy', artist: 'Kanye West', yearPublished: 2010}
     ]
-    collection.push(music)
-    return music;
+    collection = collection.concat(album)
+    return album;
 }
 console.log('testing music object',  addTooCollection());
 
 console.log('Testing collection array', collection);
 
 function showCollection(){
-   console.log('In showCollection')
-   for(let i = 0; i < collection.length; i++)
    for(let i = 0; i < collection.length; i++){
-       console.log(collection[i][i].title, 'by', collection[i][i].artist, 'published in', collection[i][i].yearPublished)
+       console.log(collection[i].title, 'by', collection[i].artist, 'published in', collection[i].yearPublished)
    }
 }
-
 console.log('In showCollection', showCollection())
 
+function findByArtist(artist){
+    let results = []
+    for(let i = 0; i < collection.length; i++){
+        if(collection[i].artist === artist){
+            results.push(artist)
+        } 
+    }
+    return results;
+}
+console.log('In findByArtist', findByArtist('Kendrick Lamar'))
+console.log('In findByArtist', findByArtist('Lil Wayne'))
 
